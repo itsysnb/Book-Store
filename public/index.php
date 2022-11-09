a95a0da0d27238cec0307bc3e7aba59a298f57ed
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-09 03:41:21 +0330

    edit index

--- public/index.php
+++ public/index.php
@@ -2,5 +2,8 @@
 
 require_once "../vendor/autoload.php";
 
-$computer = new \App\CoreI7(new \App\Computer());
-echo $computer->price();
+/*$computerCoreI7 = new \App\CoreI7(new \App\Computer());
+echo $computerCoreI7->price();\*/
+
+$computerCoreI5 = new \App\CoreI5(new \App\Computer());
+echo $computerCoreI5->price();
